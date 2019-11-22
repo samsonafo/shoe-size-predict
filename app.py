44fac7eded683a5f8973dc9a5439afba8fab35b1
Samson Afolabi <afolabisamson15@gmail.com>
2019-11-22 19:16:48 +0100

    new new

--- app.py
+++ app.py
@@ -8,27 +8,47 @@ model = pickle.load(open('model.pkl','rb'))
 # app
 app = Flask(__name__, template_folder='templates')
 
-# routes
-@app.route('/',  methods=['GET'])
+# # routes
+# @app.route('/',  methods=['GET'])
 
-def home():
-    return render_template("home.html")
+# def home():
+#     return render_template("home.html")
 
 
 
-@app.route('/result',  methods=['POST'])
-def predict():
-    if request.method == 'POST':
+# @app.route('/result',  methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
 
-        # get data and convert data into dataframe
+#         # get data and convert data into dataframe
+#         height = request.form['height']
+#         sex_no = request.form['sex_no']
+#         data_df = pd.DataFrame([[height,sex_no]],columns=['height','sex_no'])
+
+#         # predictions
+#         result = model.predict(data_df)
+#     # return data
+#     return render_template("result.html",prediction=result)
+
+# if __name__ == '__main__':
+#     app.run(port = 5000, debug=True)
+    
+    
+    
+    
+    
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('home.html')
+    
+    if flask.request.method == 'POST':
         height = request.form['height']
         sex_no = request.form['sex_no']
         data_df = pd.DataFrame([[height,sex_no]],columns=['height','sex_no'])
-
-        # predictions
         result = model.predict(data_df)
-    # return data
-    return render_template("result.html",prediction=result)
-
+        return render_template("result.html",prediction=result)
+    
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
+    
