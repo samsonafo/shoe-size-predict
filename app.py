57d7afc26781d75f49a15495a09559c699ec4552
Samson Afolabi <afolabisamson15@gmail.com>
2019-11-24 21:20:39 +0100

    final

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import pandas as pd
 from flask import Flask, jsonify, request , render_template
 import pickle
 import numpy as np
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
 
 
 # load model
