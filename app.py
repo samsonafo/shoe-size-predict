683870a5b376a45b08a8ef12e7b67d890578cb19
Samson Afolabi <afolabisamson15@gmail.com>
2019-11-24 17:06:26 +0100

    more work

--- app.py
+++ app.py
@@ -2,62 +2,36 @@ import pandas as pd
 from flask import Flask, jsonify, request , render_template
 import pickle
 import numpy as np
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-
-# # Connecting to google sheet api
-# scope = ['https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive"]
-
-# creds = ServiceAccountCredentials.from_json_keyfile_name('shoe-size-predict-406e2a882869.json', scope)
-
-# client = gspread.authorize(creds)
-
-# sheet = client.open('shoe-size-predict').sheet1
-
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
 
 # app
 app = Flask(__name__, template_folder='templates')
-
 # routes
 @app.route('/',  methods=['GET'])
-
 def home():
     return render_template("home.html")
-
-
-
 @app.route('/result',  methods=['POST'])
 def predict():
     if request.method == 'POST':
-
         # get data and convert data into dataframe
         height = request.form['height']
         sex_no = request.form['sex_no']
         data_df = pd.DataFrame([[height,sex_no]],columns=['height','sex_no'])
-
         # predictions
         output = model.predict(data_df)
         result = np.int(np.round(output))
         
     # return data
     return render_template("result.html",prediction=result)
-
 @app.route('/final', methods=['POST','GET'])
-
 def final():
     return render_template("final.html")
 
-@app.route('/get-data')
+@app.route('/get-data',  methods=['GET'])
 
-def getdata():
-#     final_data = dict(request.args)
-#     collect = [date_now()]
-#     collect += list(final_data.values())
-#     sheet.insert_row(collect, 2)
+def getdata():   
     return render_template("get-data.html")
 
 if __name__ == '__home__':
